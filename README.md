d0a282703ce3668546f69945280625dad5a3d31a
Felix Seele <fseele@gmail.com>
2020-07-26 13:54:00 +0200

    moved lzfse into its own repo

---
+++ README.md
@@ -0,0 +1,11 @@
+lzfse-rs
+======
+Rust bindings for the LZFSE reference implementation at https://github.com/lzfse/lzfse.
+
+[Documentation](https://docs.rs/lzfse)
+
+```toml
+# Cargo.toml
+[dependencies]
+lzfse = "0.1.0"
+```
--- lzfse/src/lib.rs
+++ lzfse/src/lib.rs
@@ -1,14 +1,52 @@
+//! Rust bindings for the LZFSE reference implementation
+//!
+//! https://github.com/lzfse/lzfse
+//!
+//! # Example
+//!
+//! ```
+//! use lzfse::{decode_buffer, encode_buffer};
+//!
+//! let input: &[u8] = &[0xF, 0xE, 0xE, 0xD, 0xF, 0xA, 0xC, 0xE,
+//!                      0xF, 0xE, 0xE, 0xD, 0xF, 0xA, 0xC, 0xE,
+//!                      0xF, 0xE, 0xE, 0xD, 0xF, 0xA, 0xC, 0xE,
+//!                      0xF, 0xE, 0xE, 0xD, 0xF, 0xA, 0xC, 0xE,
+//!                      0xF, 0xE, 0xE, 0xD, 0xF, 0xA, 0xC, 0xE,
+//!                      0xF, 0xE, 0xE, 0xD, 0xF, 0xA, 0xC, 0xE];
+//! 
+//! // compression
+//! // in the worst case lzfse will fallback to return the input uncompressed
+//! // and add a magic header to indicate this. that requires 12 bytes (see lzfse_encode.c)
+//! let max_outlen = input.len() + 12;
+//! let mut compressed = vec![0; max_outlen];
+//! 
+//! let bytes_out = encode_buffer(&input[..], &mut compressed[..]).unwrap();
+//! assert!(bytes_out < input.len());
+//! 
+//! // decompression
+//! // need to allocate 1 byte more since lzfse returns input.len() if the buffer is too small 🤷‍♂️
+//! let mut uncompressed = vec![0; input.len() + 1];
+//! let bytes_in = decode_buffer(&compressed[0..bytes_out], &mut uncompressed[..]).unwrap();
+//! 
+//! assert_eq!(bytes_in, input.len());
+//! assert_eq!(input[..], uncompressed[..bytes_in]);
+//! ````
+
 extern crate libc;
-extern crate lzfse_src as ffi;
+extern crate lzfse_sys as ffi;
 
 use libc::size_t;
 
+/// This type represents all possible errors that can occur when decompressing data.
 #[derive(PartialEq, Debug)]
 pub enum Error {
+    /// The buffer was not large enough for the decompressed data.
     BufferTooSmall,
+    /// Decompression failed because the input was invalid.
     CompressFailed,
 }
 
+/// Compress input into byte array
 pub fn encode_buffer(input: &[u8], output: &mut [u8]) -> Result<usize, Error> {
     let out_size = unsafe {
         ffi::lzfse_encode_buffer(
@@ -21,12 +59,13 @@ pub fn encode_buffer(input: &[u8], output: &mut [u8]) -> Result<usize, Error> {
     };
 
     if out_size == 0 {
-        return Err(Error::CompressFailed);
+        Err(Error::CompressFailed)
     } else {
-        return Ok(out_size);
+        Ok(out_size)
     }
 }
 
+/// Decompress input into byte array
 pub fn decode_buffer(input: &[u8], output: &mut [u8]) -> Result<usize, Error> {
     let out_size = unsafe {
         ffi::lzfse_decode_buffer(
@@ -39,9 +78,9 @@ pub fn decode_buffer(input: &[u8], output: &mut [u8]) -> Result<usize, Error> {
     };
 
     if out_size == output.len() {
-        return Err(Error::BufferTooSmall);
+        Err(Error::BufferTooSmall)
     } else {
-        return Ok(out_size);
+        Ok(out_size)
     }
 }
 
@@ -62,7 +101,7 @@ mod tests {
         let bytes_out = encode_buffer(&input[..], &mut compressed[..]).unwrap();
         assert_ne!(bytes_out, 0);
 
-        // need to allocate 1 byte more since lzfse returns input.len() if the buffer is too small 🤷‍♂️
+        // need to allocate 1 byte more since lzfse returns input.len() if the buffer is too small
         let mut uncompressed = vec![0; input.len() + 1];
         let bytes_in = decode_buffer(&compressed[0..bytes_out], &mut uncompressed[..]).unwrap();
 
